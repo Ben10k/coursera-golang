349c85b3e8c566bcc8ed04e72c73f63853be10b2
Ben10k <Benas.Taurosevicius@centric.eu>
2020-09-28 15:18:48 +0300

    [FEAT] add solution for week 3

--- slice/slice.go
+++ slice/slice.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	slice := make([]int, 0, 3)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("enter an integer to be added to the slice ")
+		scanner.Scan()
+		text := strings.TrimSpace(scanner.Text())
+		if text == "X" {
+			fmt.Println("input was [" + text + "], exiting")
+			break
+		}
+		integer, err := strconv.Atoi(text)
+		if err != nil {
+			fmt.Println("unable to convert text [" + text + "] to integer, ignoring")
+			continue
+		}
+		slice = append(slice, integer)
+		sort.Ints(slice)
+		fmt.Println(slice)
+	}
 }
